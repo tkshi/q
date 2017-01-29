f6e78ca3b502165b8696b4f3a2380b9b0a0e92f1
遠藤孝志 <takashi@endoutakashinoMacBook-Pro.local>
2017-01-29 20:51:25 +0900

    close #1

--- src/routes/Questionnaire/components/Counter.js
+++ src/routes/Questionnaire/components/Counter.js
@@ -1,17 +1,9 @@
 import React from 'react'
-import Modal from './Modal'
+import QuestionnaireList from './QuestionnaireList'
 
 export const Counter = (props) => (
   <div style={{ margin: '0 auto' }} >
-    <Modal {...props}/>
-    <h2>Counter: {props.counter}</h2>
-    <button className='btn btn-default' onClick={props.increment}>
-      Increment
-    </button>
-    {' '}
-    <button className='btn btn-default' onClick={props.doubleAsync}>
-      Double (Async)
-    </button>
+    <QuestionnaireList {...props}/>
     <button className='btn btn-default' onClick={props.fetchQuestionnaires}>
       fetchQuestionnaires (Async)
     </button>
@@ -19,10 +11,8 @@ export const Counter = (props) => (
 )
 
 Counter.propTypes = {
-  counter     : React.PropTypes.number.isRequired,
-  doubleAsync : React.PropTypes.func.isRequired,
+  questionnaire     : React.PropTypes.array.isRequired,
   fetchQuestionnaires : React.PropTypes.func.isRequired,
-  increment   : React.PropTypes.func.isRequired
 }
 
 export default Counter
